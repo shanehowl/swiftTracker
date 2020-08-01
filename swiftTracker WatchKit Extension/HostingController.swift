3e8eafaa09610f9238d70c7bbd7f90e4a721e826
Shane <shanehowl@hotmail.com>
2020-08-01 21:32:26 +0800

    Initial Commit

---
+++ swiftTracker WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  swiftTracker WatchKit Extension
+//
+//  Created by ho.wai.loon on 01/08/2020.
+//  Copyright © 2020 ho.wai.loon. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
---
+++ swiftTracker WatchKit Extension/NotificationView.swift
@@ -0,0 +1,21 @@
+//
+//  NotificationView.swift
+//  swiftTracker WatchKit Extension
+//
+//  Created by ho.wai.loon on 01/08/2020.
+//  Copyright © 2020 ho.wai.loon. All rights reserved.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
